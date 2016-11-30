4359049a382f149117c1114d39a2b133d60709e7
Tanmay Awasthi <tanmayawasthi105@gmail.com>
2016-11-30 22:13:38 +0530

    Mail sent feature added

--- routes/janampatri.js
+++ routes/janampatri.js
@@ -3,6 +3,8 @@
  */
 
 var Customer = require("../model/customer");
+var nodemailer=require('nodemailer');
+var smtpTransport = require('nodemailer-smtp-transport');
 exports.storeCustomer = function storeCustomer(req, res) {
     var customerName = req.body.name;
     var customerEmail = req.body.email;
@@ -27,8 +29,37 @@ exports.storeCustomer = function storeCustomer(req, res) {
         }
         else {
             console.log("Customer Added!");
+            sendMailNotification();
         }
     });
 
-    
+
+    var sendMailNotification=function(){
+        var transporter = nodemailer.createTransport(smtpTransport({
+            service: 'yahoo',
+            auth: {
+                user: 'tanmayawasthi105@yahoo.com', // Your email id
+                pass: '#Jun105#' // Your password
+            }
+        }));
+        var mailOptions = {
+            from: '"AstroGuru" <tanmayawasthi105@yahoo.com>', // sender address
+            to: 'tanmayawasthi105@yahoo.com', // list of receivers
+            subject: 'New Janampatri!!', // Subject line
+            // text: 'Hello world 🐴', // plaintext body
+            html: '<b>'+customerMessage+'</b>' // html body
+        };
+
+
+        transporter.sendMail(mailOptions, function(error, info){
+            if(error){
+                return console.log(error);
+
+            }
+            console.log('Message sent: ' + info.response);
+            res.send("Customer Added and Mail Sent!");
+        });
+
+    }
+
 }
